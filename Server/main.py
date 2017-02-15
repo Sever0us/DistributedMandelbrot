6c6b4719eb9f3fbe23f9e70fa2e94e796676b39f
EC2 Default User <ec2-user@ip-172-31-11-255.us-west-2.compute.internal>
2017-02-15 07:19:18 +0000

    fixed server bugs

--- Server/main.py
+++ Server/main.py
@@ -3,16 +3,15 @@ import socketserver
 import secrets
 
 PORT = 80
-global worker_tokens = set()
+worker_tokens = set()
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-         if self.path == '/api/register':
-             self.register()
-         else:
-         	self.error404()
-        return
+        if self.path == '/api/register':
+            self.register()
+        else:
+            self.error404()
 
     def register(self):
     	# Create new token and add to list of workers
@@ -21,7 +20,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(new_token)
+        self.wfile.write(new_token.encode())
 
         global worker_tokens
         worker_tokens.add(new_token)
@@ -30,8 +29,8 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_response(404)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write('<html><head><title>404 Error</title></head>')
-        self.wfile.write('<body><h1>404 Not found</h1></body></html>')
+        self.wfile.write('<html><head><title>404 Error</title></head>'.encode())
+        self.wfile.write('<body><h1>404 Not found</h1></body></html>'.encode())
 
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
